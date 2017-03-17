d6bc1860714d7d91e1a61811844dc2c2cd5993d9
utku <uuacikalin@gmail.com>
2017-03-17 22:45:33 +0300

    Must have method definitions.

--- plugins/plugin-java/che-plugin-java-ext-lang-client/src/main/java/org/eclipse/che/ide/ext/java/client/refactoring/extractMethod/ExtractMethodRefactoringAction.java
+++ plugins/plugin-java/che-plugin-java-ext-lang-client/src/main/java/org/eclipse/che/ide/ext/java/client/refactoring/extractMethod/ExtractMethodRefactoringAction.java
@@ -1,4 +1,35 @@
-@Singleton
-public class ExtractMethodRefactoringAction extends AbstractPerspectiveAction implements ActivePartChangedHandler {
-	//starting poing
+package org.eclipse.che.ide.ext.java.client.refactoring.extractMethod;
+
+import org.eclipse.che.commons.annotation.Nullable;
+import org.eclipse.che.ide.api.action.AbstractPerspectiveAction;
+import org.eclipse.che.ide.api.action.ActionEvent;
+import org.eclipse.che.ide.api.event.ActivePartChangedEvent;
+import org.eclipse.che.ide.api.event.ActivePartChangedHandler;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+/**
+ * Created by Utku on 13.03.2017.
+ */
+public class ExtractMethodRefactoringAction extends AbstractPerspectiveAction implements ActivePartChangedHandler{
+
+    public ExtractMethodRefactoringAction(@Nullable List<String> perspectives, @NotNull String text, @NotNull String description) {
+        super(perspectives, text, description);
+    }
+
+    @Override
+    public void onActivePartChanged(ActivePartChangedEvent event) {
+
+    }
+
+    @Override
+    public void updateInPerspective(@NotNull ActionEvent event) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
